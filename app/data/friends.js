473842f02969bf6debae7740d89e5f5ecb262027
erharmon <emerson.harmon@gmail.com>
2019-05-27 19:34:05 -0400

    basic compatibility logic in place, must add supporting html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,16 @@
-var matches = [];
+var matches = [
+    {
+        name: 'Fred',
+        responses: [
+            1, 2, 3
+        ]
+    },
+    {
+        name: 'Abigail',
+        responses: [
+            5, 5, 5
+        ]
+    }
+];
 
 module.exports = matches;
