092b2a3e545c61699885af20a7c9012b3ec3f570
erharmon <emerson.harmon@gmail.com>
2019-05-28 19:48:28 -0400

    trying to resolve port and hostname issues

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require("path");
 
 var port = process.env.port || 2019;
-
+var host = process.env.host || '0.0.0.0';
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -11,6 +11,6 @@ app.use(express.json());
 require("./app/routing/htmlRoutes")(app);
 require('./app/routing/apiRoutes')(app);
 
-app.listen(port, function() {
+app.listen(port, host, function() {
   console.log("Server started on port " + port);
 });
