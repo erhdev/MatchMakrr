af5fe11549cbdd768c766981aeac5f2819601396
erharmon <emerson.harmon@gmail.com>
2019-05-28 20:18:27 -0400

    dotting heroku i's and crossing heroku t's

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,4 +6,7 @@ app.get("/home", function(req, res) {
 app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
+app.get("/" , function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
 }
