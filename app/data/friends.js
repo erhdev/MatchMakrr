096870ed6373e1d46b0818a561b4df4180e2d2c3
erharmon <emerson.harmon@gmail.com>
2019-05-28 20:33:47 -0400

    added new starter date option + more questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,14 +3,21 @@ var matches = [
         name: 'Fred',
         photo: 'https://vignette.wikia.nocookie.net/scoobydoo/images/3/36/Cyber_Fred_%28Cyber_Chase%29.png/revision/latest?cb=20160227044158',
         responses: [
-            1, 2, 3
+            1, 2, 3, 4, 5
         ]
     },
     {
         name: 'Velma',
         photo: 'https://imgix.ranker.com/user_node_img/50077/1001522560/original/velma-is-a-lesbian-she-just-hasn_t-come-out-yet-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces',
         responses: [
-            5, 5, 5
+            5, 5, 5, 5, 5
+        ]
+    },
+    {
+        name: 'Shaggy',
+        photo: 'https://images.ecosia.org/QmspF30s5OazBznT6H3xMQBVCqg=/0x390/smart/http%3A%2F%2F4.bp.blogspot.com%2F-aGbJ1bXQjKE%2FUNWi61DhQFI%2FAAAAAAAAIw4%2Fzs4pKCe2fUI%2Fs1600%2FScooby%2BMI_Shaggy_Head%2BShot_w%2BBG.jpg',
+        responses: [
+            1, 1, 1, 1, 1
         ]
     }
 ];
